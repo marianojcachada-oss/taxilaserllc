7bbf2da6ea900fa0eaaa5afa076426e5cbf1b108
marianojcachada-oss <marianojcachada@gmail.com>
2025-12-11 16:42:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(express.json());
 
 // Credenciales del bot de Azure
 const adapter = new BotFrameworkAdapter({
-    appId: process.env.a2096582-bf9e-4520-ab9d-9d51d1ea1be1,
-    appPassword: process.env.53e694f5-9c69-49cd-b644-9c06bfb22463
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Bot logic
@@ -19,7 +19,7 @@ adapter.onTurn(async (context) => {
         if (text === '/crearreporte') {
             try {
                 // Llamada al flujo Power Automate
-                const respuesta = await fetch(process.env."https://defaultc8aebb6cc6aa405da76b057a64fbdc.17.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/dfddeee54d7e4ec7b070b66cbb27e3c8/triggers/manual/paths/invoke?api-version=1", {
+                const respuesta = await fetch(process.env.PA_FLOW_URL, {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
@@ -50,3 +50,4 @@ app.post('/api/messages', (req, res) => {
 
 // Start server
 app.listen(3000, () => console.log('Bot escuchando en puerto 3000'));
+
